0a30a80c3d4069a2bea2ed690fab53aa569ba2e8
edramos-lab <edramos.us@gmail.com>
2025-05-28 13:35:27 -0700

    Updated ReduceLROnPlateau scheduler: factor=0.5, verbose=False

--- train.py
+++ train.py
@@ -307,7 +307,7 @@ def main():
             
             # Initialize optimizer, scheduler and criterion
             optimizer = optim.Adam(model.parameters(), lr=args.lr)
-            scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=3, factor=0.1)
+            scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, verbose=False)
             criterion = nn.CrossEntropyLoss()
             
             # Training loop
@@ -537,7 +537,7 @@ def main():
             
             # Initialize optimizer, scheduler and criterion
             optimizer = optim.Adam(model.parameters(), lr=args.lr)
-            scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=3, factor=0.1)
+            scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, verbose=False)
             criterion = nn.CrossEntropyLoss()
             
             # Training loop
